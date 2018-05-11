a38831f2940cad6e2f42c6bddd701d62010860ab
alexshatalin <devops178@gmail.com>
2018-05-10 22:20:39 -0400

    Setup project and started on header

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Vintage Twins</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/nav_menu.js"></script>
+</head>
+<body>
+	<div class="__dev__"></div>
+	<div class="page_container">
+		<header class="main_header">
+			<div class="main_header__promo_bar"><span>50% sale.</span>just today</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="main_header__menu_button"></div>
+						<div class="main_header__logo"><img src="images/logo.png" alt="The Vintage Twin" /></div>
+						<div class="main_header__nav">
+							<ul>
+								<li class="active"><a href="#">clothing</a>
+									<ul class="submenu">
+										<li data-id="unisex">unisex</li>
+										<li data-id="more">more</li>
+										<li data-id="trending">trending</li>
+									</ul>
+								</li>
+								<li><a href="#">accessories</a></li>
+								<li><a href="#">home</a></li>
+								<li><a href="#">trending</a></li>
+								<li><a href="#">the vault</a></li>
+								<li><a href="#">jeanius bar</a></li>
+							</ul>
+						</div>
+						<div class="main_header__secondary_nav">
+							<ul>
+								<li class="search"></li>
+								<li class="account">account</li>
+								<li class="wishlist"></li>
+								<li class="cart"></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	</div>
+</body>
+</html>
