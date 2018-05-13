d5b09e6738503a17e0e6539283c27a58eb5f9a5b
alexshatalin <devops178@gmail.com>
2018-05-13 18:15:57 -0400

    Cut up lyout into sections

--- index.php
+++ index.php
@@ -11,85 +11,11 @@
 <body>
 	<div class="__dev__"></div>
 	<section class="page_container">
-		<header class="main_header">
-			<div class="main_header__promo_bar"><div class="text"><span>50% sale.</span>just today</div></div>
-			<div class="bottom_nav_content">
-				<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<div data-id="mobile_nav_button" class="main_header__menu_button"><span></span></div>
-						<div class="main_header__logo"><img src="images/logo.png" alt="The Vintage Twin" /></div>
-						<div class="main_header__nav">
-							<nav>
-								<ul>
-									<li class="active"><a href="#">clothing</a>
-										<ul class="submenu">
-											<li data-id="unisex">unisex</li>
-											<li data-id="more">more</li>
-											<li data-id="trending">trending</li>
-										</ul>
-									</li>
-									<li><a href="#">accessories</a></li>
-									<li><a href="#">home</a></li>
-									<li><a href="#">trending</a></li>
-									<li><a href="#">the vault</a></li>
-									<li><a href="#">jeanius bar</a></li>
-								</ul>
-							</nav>
-						</div>
-						<div class="main_header__secondary_nav">
-							<nav>
-								<ul>
-									<li><i class="fas fa-search"></i></li>
-									<li class="account">account</li>
-									<li><i class="far fa-heart"></i></li>
-									<li class="cart"><i class="fas fa-shopping-bag"></i></li>
-								</ul>
-							</nav>
-						</div>
-					</div>
-				</div>
-				</div>
-			</div>
-		</header>
+		<? require_once(__DIR__ . '/layout/header.php');?>
 		<section class="page_content">
 			<main><img src="dev/temp_home_d.jpg"></main>
 		</section>
-		<footer class="main_footer">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="main_footer__content">
-							<dl class="main_footer__left_nav">
-								<dt>customer care</dt>
-								<dd><a href="#">return policy</a></dd>
-								<dd><a href="#">shipping info</a></dd>
-								<dd><a href="#">contact us</a></dd>
-								<dd><a href="#">special requests</a></dd>
-								<dd><a href="#">privacy</a></dd>
-								<dd><a href="#">terms</a></dd>
-							</dl>
-							<dl class="main_footer__left_nav">
-								<dt>company</dt>
-								<dd><a href="#">about us</a></dd>
-								<dd><a href="#">faq</a></dd>
-								<dd><a href="#">charity</a></dd>
-								<dd><a href="#">press</a></dd>
-								<dd><a href="#">feedback</a></dd>
-								<dd><a href="#">wholesale</a></dd>
-							</dl>
-							<dl class="main_footer__right_nav">
-								<dt>#something</dt>
-								<dd><a href="#"><i class="fab fa-twitter"></i></a></dd>
-								<dd><a href="#"><i class="fab fa-facebook-f"></i></a></dd>
-								<dd><a href="#"><i class="fab fa-instagram"></i></a></dd>
-							</dl>
-							<p>&copy; the vintage twin. llc 2018</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
+		<? require_once(__DIR__ . '/layout/footer.php');?>
 	</section>
 </body>
 </html>
---
+++ layout/footer.php
@@ -0,0 +1,35 @@
+<footer class="main_footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="main_footer__content">
+					<dl class="main_footer__left_nav">
+						<dt>customer care</dt>
+						<dd><a href="#">return policy</a></dd>
+						<dd><a href="#">shipping info</a></dd>
+						<dd><a href="#">contact us</a></dd>
+						<dd><a href="#">special requests</a></dd>
+						<dd><a href="#">privacy</a></dd>
+						<dd><a href="#">terms</a></dd>
+					</dl>
+					<dl class="main_footer__left_nav">
+						<dt>company</dt>
+						<dd><a href="#">about us</a></dd>
+						<dd><a href="#">faq</a></dd>
+						<dd><a href="#">charity</a></dd>
+						<dd><a href="#">press</a></dd>
+						<dd><a href="#">feedback</a></dd>
+						<dd><a href="#">wholesale</a></dd>
+					</dl>
+					<dl class="main_footer__right_nav">
+						<dt>#something</dt>
+						<dd><a href="#"><i class="fab fa-twitter"></i></a></dd>
+						<dd><a href="#"><i class="fab fa-facebook-f"></i></a></dd>
+						<dd><a href="#"><i class="fab fa-instagram"></i></a></dd>
+					</dl>
+					<p>&copy; the vintage twin. llc 2018</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
